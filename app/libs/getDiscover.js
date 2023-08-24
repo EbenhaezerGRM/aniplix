7eba698ca4f2dc3afde0c6ba706799cf4ba5edd9
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-08-24 11:19:26 +0700

    fix the "failed to fetch"

--- app/libs/getDiscover.js
+++ app/libs/getDiscover.js
@@ -1,5 +1,5 @@
 export default async function getDiscover(){
-    const response = await fetch(`${process.env.GOGOANIME_BASE_URL}/new-season`, 
+    const response = await fetch(`http://localhost:5000/new-season`, 
     {cache: 'no-cache'})
     
     if(!response.ok){
