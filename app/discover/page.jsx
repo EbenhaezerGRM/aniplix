dda850eeef220138ee8f5feb6c5ab220473faca8
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-08-24 11:16:52 +0700

    Filter Anime

--- app/discover/page.jsx
+++ app/discover/page.jsx
@@ -1,17 +1,43 @@
-import { RiCompassDiscoverLine } from 'react-icons/ri'
+import { RiCompassDiscoverLine } from 'react-icons/ri';
+import { AiFillFilter } from 'react-icons/ai';
+
 const discoverPage = () => {
   return (
     <div className='componentSpacing'>
       <div className='flex-center gap-4'>
-        <RiCompassDiscoverLine
-          size={40}
-          />
+        <RiCompassDiscoverLine size={40} />
         <div>
           <p className='font-bold text-xl'>Discover</p>
         </div>
       </div>
-    </div>
-  )
-}
 
-export default discoverPage
+      <div className='mt-2 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2'>
+        <div className='flex flex-wrap'>
+          <input
+            placeholder='Search...'
+            className='rounded-md focus:outline-none text-slate-900 py-2 px-4 w-full'
+          />
+        </div>
+
+        <div className='py-2 px-4 bg-slate-900 rounded-md items-center flex font-bold'>
+          Genre
+        </div>
+
+        <div className='py-2 px-4 bg-slate-900 rounded-md items-center flex font-bold'>
+          Language
+        </div>
+
+        <div className='py-2 px-4 bg-slate-900 rounded-md items-center flex font-bold'>
+          Status
+        </div>
+
+        <button className='py-2 px-4 bg-teal-500 rounded-md items-center flex font-bold text-slate-900 gap-2 justify-center'>
+          Filter
+          <AiFillFilter />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default discoverPage;
