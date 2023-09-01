3ed027c6b753d7b7d13efc77ba346ac650753b21
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-09-01 12:07:08 +0700

    DiscoverPage

--- app/discover/page.jsx
+++ app/discover/page.jsx
@@ -1,7 +1,33 @@
-import { RiCompassDiscoverLine } from 'react-icons/ri';
-import { AiFillFilter } from 'react-icons/ai';
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { getDiscover } from '@app/libs/getDiscover'
+import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai'
+import { RiCompassDiscoverLine } from 'react-icons/ri'
+import { AiFillFilter } from 'react-icons/ai'
 
-const discoverPage = () => {
+export default function discoverPage () {
+  const [discover, SetDiscover] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+
+  useEffect(() => {
+    fetchDiscover(currentPage)
+}, [currentPage])
+
+async function fetchDiscover(page) {
+  const trendingResults = await getDiscover(page)
+  SetDiscover(trendingResults)
+}
+
+function handleNextPage() {
+  setCurrentPage((prevPage) => prevPage + 1)
+}
+
+function handlePreviousPage() {
+  setCurrentPage((prevPage) => prevPage - 1)
+}
+
   return (
     <div className='componentSpacing'>
       <div className='flex-center gap-4'>
@@ -35,9 +61,47 @@ const discoverPage = () => {
           Filter
           <AiFillFilter />
         </button>
-      </div>
-    </div>
-  );
-};
 
-export default discoverPage;
+        <div className='flex justify-center items-center'>
+          <button
+              className="pageButton"
+              disabled={currentPage === 1}
+              onClick={handlePreviousPage}
+              > 
+              <AiFillCaretLeft/>
+            </button>
+            <p className='px-4'>{currentPage}</p>
+            <button
+              className="pageButton"
+              onClick={handleNextPage}
+              >
+              <AiFillCaretRight/>
+            </button>
+        </div>
+
+      </div>
+
+      {discover.map((result) => (
+        <div className='mt-4 rounded-lg hover:bg-gray-800'>
+          <Link
+            href={result.animeUrl}
+            >
+            <div className='flex'>
+              <Image
+                className='rounded-lg w-24 lg:w-48 object-cover'
+                src={result.animeImg}
+                alt={result.animeTitle || result.animeId}
+                width={200}
+                height={200}
+                />
+              <div className='px-4'>
+                <p className='font-bold lg:text-xl'>{result.animeTitle}</p>
+                <p className='my-2'>{result.releasedDate}</p>
+              </div>
+            </div>
+            </Link>
+          </div>
+      ))}
+    </div>
+  )
+}
